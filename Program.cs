7543baac694025ee29397ad980eab806adf81419
Jan Leirvaag <lja022025@gmail.com>
2025-03-18 12:29:59 +0100

    new

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? value = "";
+
+        //these two variables can be constants since they will never change
+        const string exit = "exit";
+        const string message = "Your passwordlength is";
+
+        // do
+        // {
+        while (value.ToLower() != exit)
+        {
+            Console.WriteLine("\nPlease type in your password (or 'exit' to quit): ");
+            value = Console.ReadLine();
+
+            //check for empty pw
+            if (!string.IsNullOrEmpty(value) && value.ToLower() != exit)
+            {
+
+                switch (value.Length)
+                {
+                    case > 15:
+                        Console.WriteLine($"{message} {value.Length}. Brilliant!");
+                        break;
+
+                    case > 10:
+                        Console.WriteLine($"{message} {value.Length}. A decent size");
+                        break;
+
+                    case < 10:
+                        Console.WriteLine($"{message} {value.Length}. A bit short");
+                        break;
+                }
+            }
+        }
+    }
+}
