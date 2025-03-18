b050a19416dd90abaad55911229f4e85d08259f6
Jan Leirvaag <lja022025@gmail.com>
2025-03-18 16:20:32 +0100

    fixed loop bug

--- Program.cs
+++ Program.cs
@@ -4,23 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        //make string variable a nullable reference type (whatever that is)
+        //Make string variable a nullable reference type (whatever that is)
+        //Appears to have something to do with Readline()
         string? value;
 
-        //these variables can be constants since they will never change
+        //These variables can be constants since they will never change
         const string exit = "exit";
         const string message = "Your passwordlength is";
-        const string typeInPW = "\nPlease type in your password (or 'exit' to quit): ";
+        const string typeInPW = "Please type in your password (or 'exit' to quit): ";
 
         do
         {
             Console.WriteLine(typeInPW);
             value = Console.ReadLine();
 
-            //check for empty value and that value is not 'exit' (which will exit the program)
-            if (!string.IsNullOrEmpty(value) && value.ToLower() != exit)
+            //Check for empty/null value 
+            if (!string.IsNullOrEmpty(value))
             {
-                //choose cases based on value.length
+
+                //Exit loop if value == exit
+                if (value.ToLower() == exit)
+                    break;
+
+                //Choose cases based on value.length
                 switch (value.Length)
                 {
                     case > 15:
@@ -36,6 +42,12 @@ class Program
                         break;
                 }
             }
-        } while (value.ToLower() != exit);
+            else
+            {
+                //Inform user if value is empty/null
+                Console.WriteLine($"Minimum passwordlength is 1.");
+                continue;
+            }
+        } while (true);
     }
 }
--- obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c75db3d1a58ac462b85db664e0b4dff1786581")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa1a9023b1bbe5e07f5437d16ed466bdc1123c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
